4cb8ba6ca585657bccba0933062ea5f5f2c40326
Boozy <381658945@qq.com>
2018-04-28 22:30:38 +0800

    Chained all shells in test_them_all.sh

--- test_them_all.sh
+++ test_them_all.sh
@@ -1,4 +1,5 @@
-./test_with_clang_3_8.sh
-./test_with_gcc_4_9.sh
-./test_with_default_clang.sh
+./clean_test_build.sh
+./test_with_clang_3_8.sh &&
+./test_with_gcc_4_9.sh &&
+./test_with_default_clang.sh &&
 ./test_with_default_gcc.sh
