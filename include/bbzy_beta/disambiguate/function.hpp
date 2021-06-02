323eef069e71946eb10c1e1973b3267f583c709b
bbzy <381658945@qq.com>
2021-06-02 22:23:10 +0800

    Add docs to function.hpp and reformat

--- include/bbzy_beta/disambiguate/function.hpp
+++ include/bbzy_beta/disambiguate/function.hpp
@@ -7,20 +7,26 @@ namespace bbzy
 {
 namespace disam
 {
-namespace detail
-{
+/**
+ * Disambiguate from overloaded functions.
+ * @tparam ReturnT Return type.
+ * @tparam FuncParamTs Parameter types.
+ * @remarks
+ *      When you have:
+ *          void foo(int);
+ *          void foo(char);
+ *      and you want:
+ *          &foo
+ *      The compile would tell you ambiguated function reference, because the compile does not even know which overloaded function the code should be used.
+ *      Then we can use Function<void, int>::get(&foo) to get the 1st foo() or use Function<void, char>::get(&foo) to get the 2nd foo()
+ */
 template<class ReturnT, class... FuncParamTs>
 struct Function
 {
-    static auto get(ReturnT(* func)(FuncParamTs...)) ->
-    ReturnT(*)(FuncParamTs...)
+    static auto get(ReturnT(* func)(FuncParamTs...)) -> ReturnT(*)(FuncParamTs...)
     {
         return func;
     }
 };
-}
-
-using detail::Function;
-
 }
 }
